fabd7aad6b7ecd8c3ea4716fcbc179fdf32683e1
VLV01 <vinicius.linsvasco@hotmail.com>
2022-07-15 13:50:27 -0300

    fix

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -29,21 +29,19 @@ public class SmsService {
 	private SaleRepository saleRepository;
 
 	public void sendSms(Long saleId) {
-		
-		Sale sale = saleRepository.findById(saleId).get();
-		
-		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		
-		String msg = " O vendedor " + sale.getSellerName() + " foi destaque em " + date
-				+ " com um total de R$	" + String .format("%.2f", sale.getAmount());
 
-		Twilio.init(twilioSid, twilioKey);
+        Sale sale = saleRepository.findById(saleId).get();
+        String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+        String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+                + " com um total de R$ " + String.format("%.0f", sale.getAmount());
 
-		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+        Twilio.init(twilioSid, twilioKey);
 
-		Message message = Message.creator(to, from, msg).create();
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-		System.out.println(message.getSid());
+        Message message = Message.creator(to, from, msg).create();
+
+        System.out.println(message.getSid());
 	}
 }
